687f6a2053a7bd3ad1cf78b16e2e6415bd267aa8
cpp0330 <1449342462@qq.com>
2020-04-12 11:09:22 +0800

    todolist

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './style.css'
+class Header extends Component {
+    render() {
+        return (
+            <header className='header'>
+                头部
+            </header>
+        );
+    }
+}
+
+export default Header;
